ab9b81dee32b81c1492238587e36642daa769fa7
Chetos26 <rlz.romero@yavirac.edu.ec>
2023-08-18 07:00:49 -0500

    uservalidation

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -4,7 +4,6 @@ import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from './entities/users.entity';
 import { UsersDto } from './dto/users.dto';
-import { CreateUsersDto } from './dto/create-users.dto';
 import { ErrorManager } from 'src/utils/error.manager';
 import { UpdateUsersDto } from './dto/update-users.dto';
 
@@ -14,17 +13,72 @@ export class UsersService {
   constructor(
     @InjectRepository(Users) private usersRepository: Repository<Users>
     ){}
-    
 
-  async create(createusersDto: UsersDto):Promise<Users> {
-    try {
-      console.log(createusersDto)
-        const users: Users = await this.usersRepository.save(createusersDto);
+    async checkDuplicateEmail(email: string): Promise<boolean> {
+      const existingUser = await this.usersRepository.findOne({ where: { email } });
+      return !!existingUser;
+    }
+  
+    async checkDuplicatePhone(phone: string): Promise<boolean> {
+      const existingUser = await this.usersRepository.findOne({ where: { telf: phone } });
+      return !!existingUser;
+    }
+    
+    async create(usersDto: UsersDto): Promise<Users> {
+      const { email, telefono } = usersDto;
+  
+      if (await this.checkDuplicateEmail(email)) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'El email ya está registrado',
+        });
+      }
+  
+      if (await this.checkDuplicatePhone(telefono)) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'El número de teléfono ya está registrado',
+        });
+      }
+  
+      try {
+        const users: Users = await this.usersRepository.save(usersDto);
         return users;
       } catch (e) {
         throw new Error(e);
+      }
     }
-  }
+  
+    async update(id_u: string, updateUsersDto: UpdateUsersDto): Promise<UpdateResult | undefined> {
+      const { email, telefono } = updateUsersDto;
+  
+      if (await this.checkDuplicateEmail(email)) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'El email ya está registrado',
+        });
+      }
+  
+      if (await this.checkDuplicatePhone(telefono)) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'El número de teléfono ya está registrado',
+        });
+      }
+  
+      try {
+        const users: UpdateResult = await this.usersRepository.update(id_u, updateUsersDto);
+        if (users.affected === 0) {
+          throw new ErrorManager({
+            type: 'BAD_REQUEST',
+            message: 'No se ha podido actualizar',
+          });
+        }
+        return users;
+      } catch (e) {
+        throw ErrorManager.createSignatureError(e.message);
+      }
+    }
 
   async findAll():Promise<Users[]> {
     try {
@@ -56,7 +110,7 @@ export class UsersService {
     }
   }
   
-  async update(id_u: string, updateUsersDto: UpdateUsersDto): Promise<UpdateResult | undefined> {
+  /*async update(id_u: string, updateUsersDto: UpdateUsersDto): Promise<UpdateResult | undefined> {
     try {
       const users: UpdateResult = await this.usersRepository.update(id_u, updateUsersDto);
       if (users.affected === 0) {
@@ -69,7 +123,7 @@ export class UsersService {
     } catch (e) {
       throw ErrorManager.createSignatureError(e.message);
     }
-  }
+  }*/
 
   async remove(id_u: string):Promise<DeleteResult | undefined> {
     try {
